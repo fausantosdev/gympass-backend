773ceef1619701abfc1aa581d4dfd0bdc6c51f94
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-12-16 22:52:43 +0000

    fix: correc 'create' method name

--- src/repositories/prisma-users-repository.ts
+++ src/repositories/prisma-users-repository.ts
@@ -2,7 +2,7 @@ import { Prisma } from '@prisma/client'
 import { prisma } from '../lib/prisma'
 
 class PrismaUsersRepository {
-  async creste(data: Prisma.UserCreateInput)  {
+  async create(data: Prisma.UserCreateInput)  {
     const user = await prisma.user.create({
       data
     })
