daa3b073b7e83a0393ad14977c1ad56f0c785c69
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-12-19 22:51:21 +0000

    test: should hash user password upon registration

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,25 +1,12 @@
 import { expect, describe, it } from 'vitest'
 import { RegisterUser } from './register'
 import { compare } from 'bcryptjs'
+import { InMemoryUserRepository } from '@/repositories/in-memory/in-memory-user-repository'
 
 describe('Register use case', () => {
   it('should hash user password upon registration', async () => {
-    const registerUseCase = new RegisterUser({
-      async create(data) {
-        return {
-          id: 'user-1',
-          name: data.name,
-          email: data.email,
-          password_hash: data.password_hash,
-          created_at: new Date(),
-          updated_at: new Date()
-        }
-      },
-
-      async findByEmail(email) {
-        return null
-      },
-    })
+    const inMemoryUserRepository = new InMemoryUserRepository()
+    const registerUseCase = new RegisterUser(inMemoryUserRepository)
 
     const { user } = await registerUseCase.execute({
       name: 'John Doe',
