48e437d76971d3638a3723b6df56490c0bb62061
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-12-27 17:19:19 +0000

    feat: creates the authentication use case

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,38 @@
+import { compare } from 'bcryptjs'
+
+import { UsersRepository } from '@/repositories/users-repository'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+type Params = {
+  email: string
+  password: string
+}
+
+type AuthenticateResponse = {
+  user: {
+    id: string
+    name: string
+    email: string
+    password_hash: string
+    created_at: Date
+    updated_at: Date
+  }
+}
+
+export class Authenticate {
+  constructor(
+    private usersRepository: UsersRepository
+  ) {}
+
+  async execute({ email, password }: Params): Promise<AuthenticateResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if(!user) throw new InvalidCredentialsError()
+
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+    if(!doesPasswordMatches) throw new InvalidCredentialsError()
+
+    return { user }
+  }
+}
