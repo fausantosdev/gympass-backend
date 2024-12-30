f5c01e8dd9ef7d4dfada699886983cc6a5a65e8c
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-12-30 12:04:43 +0000

    fix: imports the authentication use case

--- src/use-cases/factories/make-authenticate-use-case.ts
+++ src/use-cases/factories/make-authenticate-use-case.ts
@@ -1,9 +1,9 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { RegisterUser } from '../register'
+import { Authenticate } from '../authenticate'
 
 export function makeAuthenticateUseCase() {
   const usersRepository = new PrismaUsersRepository()
-  const registerUser = new RegisterUser(usersRepository)
+  const registerUser = new Authenticate(usersRepository)
 
   return registerUser
 }
