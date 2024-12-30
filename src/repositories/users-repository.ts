3c3719ca6080b631a677b72a95845ce1fa127857
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-12-30 13:13:24 +0000

    feat: get user profile

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -16,6 +16,7 @@ type CreateUserType = {
 interface UsersRepository {
   create(data: CreateUserType): Promise<UserType>
   findByEmail(email: string): Promise<UserType | null>
+  findById(id: string): Promise<UserType | null>
 }
 
 export {
---
+++ src/use-cases/get-user-profile.ts
@@ -0,0 +1,31 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+type Params = {
+  userId: string
+}
+
+type GetUserProfileResponse = {
+  user: {
+    id: string
+    name: string
+    email: string
+    password_hash: string
+    created_at: Date
+    updated_at: Date
+  }
+}
+
+export class GetUserProfile {
+  constructor(
+    private usersRepository: UsersRepository
+  ) {}
+
+  async execute({ userId }: Params): Promise<GetUserProfileResponse> {
+    const user = await this.usersRepository.findById(userId)
+
+    if(!user) throw new ResourceNotFoundError()
+
+    return { user }
+  }
+}
