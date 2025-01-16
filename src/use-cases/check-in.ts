e22fbbe2c2a9d47b0f2c9e5c5c8491f723228468
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-01-16 00:17:46 +0000

    feat: add validation to prevent multiple check-ins on the same day

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,5 +1,5 @@
 import { CheckIn as CheckInModel } from '@prisma/client'
-import { CheckInRepository } from '@/repositories/check-in-repository'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
 
 type Params = {
   userId: string
@@ -12,11 +12,15 @@ type CheckInResponse = {
 
 export class CheckIn {
   constructor(
-    private checkInRepository: CheckInRepository
+    private checkInsRepository: CheckInsRepository
   ) {}
 
   async execute({ userId, gymId }: Params): Promise<CheckInResponse> {
-    const checkIn = await this.checkInRepository.create({
+    const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(userId, new Date())
+
+    if(checkInOnSameDay) throw new Error()
+
+    const checkIn = await this.checkInsRepository.create({
       userId,
       gymId
     })
