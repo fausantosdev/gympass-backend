630cd333a561651182920876b8bf94ee98ea6954
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-01-16 00:18:05 +0000

    test: add validation to prevent multiple check-ins on the same day

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,6 +1,6 @@
 import { randomUUID } from 'node:crypto'
 
-import { expect, describe, it, beforeEach } from 'vitest'
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 
 import { InMemoryCheckInRepository } from '@/repositories/in-memory/in-memory-check-in-repository'
 import { CheckIn } from './check-in'
@@ -12,6 +12,12 @@ describe('Check-in use case', () => {
   beforeEach(() => {
     inMemoryUserRepository = new InMemoryCheckInRepository()
     sut = new CheckIn(inMemoryUserRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to check in', async () => {
@@ -21,5 +27,22 @@ describe('Check-in use case', () => {
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2023, 0, 20, 8, 0, 0))
+
+    const userId = randomUUID()
+    const gymId = randomUUID()
+
+    await sut.execute({
+      userId,
+      gymId
+    })
+
+    await expect(() => sut.execute({
+      userId,
+      gymId
+    })).rejects.toBeInstanceOf(Error)
   })
 })
