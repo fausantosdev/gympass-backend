bca25e1aab867d34d8417b84e2c311e78790b272
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-04-03 16:50:08 +0000

    feat(check-in): add expiration rule (valid for 20 minutes after creation)

--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -4,6 +4,8 @@ import { ResourceNotFoundError } from './errors/resource-not-found-error'
 import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 import { MaxNumbersOfCkeckInsError } from './errors/max-numbers-of-check-ins-error'
 import { MaxDistanceError } from './errors/max-distance-error'
+import dayjs from 'dayjs'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
 
 type ValidateCheckInRequest = {
   checkInId: string
@@ -22,6 +24,15 @@ export class ValidateCheckIn {
     const checkIn = await this.checkInsRepository.findById(checkInId)
 
     if(!checkIn) throw new ResourceNotFoundError()
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes'
+    )
+
+    if(distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError()
+    }
 
     checkIn.validated_at = new Date()
 
