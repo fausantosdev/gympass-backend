85215a5378d6270cebfbbc82aead29e8aaadcb0b
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-05-27 00:38:43 +0000

    feat(users): add findById method to PrismaUsersRepository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,6 +3,14 @@ import { prisma } from '../../lib/prisma'
 import { UsersRepository } from '../users-repository'
 
 class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: { id }
+    })
+
+    return user
+  }
+
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data
