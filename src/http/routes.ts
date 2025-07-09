fe5e083e0b1e8b627e38dd395d8a74d2d81749bb
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-07-09 23:39:45 +0000

    refactor: extract JWT verification into middleware

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -4,10 +4,12 @@ import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
 
+import { verifyJwt } from './middlewares/verify-jwt'
+
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
   // Authenticated
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [ verifyJwt ] }, profile)
 }
