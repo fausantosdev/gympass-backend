336453d3be631d09d47625b765d44b9ec79a43c7
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-08-26 23:45:59 +0000

    feat: create check-in validation controller

---
+++ src/http/controllers/check-ins/validate.ts
@@ -0,0 +1,20 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { makeValidateCheckInUseCase } from '@/use-cases/factories/make-validade-check-in-use-case'
+
+export async function validate(request: FastifyRequest, reply: FastifyReply) {
+  const paramsSchema = z.object({
+    checkInId: z.string().uuid()
+  })
+
+  const { checkInId } = paramsSchema.parse(request.params)
+
+  const validateCheckIn = makeValidateCheckInUseCase()
+
+  await validateCheckIn.execute({
+    checkInId
+  })
+
+  return reply.status(201).send()
+}
