2d7e594d17ae75eeba53b9c6cb00182eef1878be
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-08-26 23:47:38 +0000

    feat: creates the routes related to the check-in controllers

---
+++ src/http/controllers/check-ins/routes.ts
@@ -0,0 +1,17 @@
+import { FastifyInstance } from 'fastify'
+
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
+
+import { create } from './create'
+import { validate } from './validate'
+import { history } from './history'
+import { metrics } from './metrics'
+
+export async function checkInsRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJwt)
+
+  app.post('check-ins/:gymId', create)
+  app.patch('check-ins/history', history)
+  app.patch('check-ins/metrics', metrics)
+  app.patch('check-ins/:checkInId/validate', validate)
+}
