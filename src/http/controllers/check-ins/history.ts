07f809b7c0e35cb25317bf931115dadee92c93b9
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-08-26 23:43:32 +0000

    feat: create check-in history controller

---
+++ src/http/controllers/check-ins/history.ts
@@ -0,0 +1,27 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { makeFetchUserCheckInsHistoryUseCase } from '@/use-cases/factories/make-fetch-user-checkins-history-use-case'
+
+export async function history(request: FastifyRequest, reply: FastifyReply) {
+  const queryParamsSchema = z.object({
+    page: z
+      .coerce
+      .number()
+      .min(1)
+      .default(1)
+  })
+
+  const { page } = queryParamsSchema.parse(request.query)
+
+  const fetchUserCheckInsHistory = makeFetchUserCheckInsHistoryUseCase()
+
+  const checkIns = await fetchUserCheckInsHistory.execute({
+    userId: String(request.user.sub),
+    page
+  })
+
+  return reply.status(201).send({
+    checkIns
+  })
+}
