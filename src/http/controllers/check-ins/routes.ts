47522b154d7119cbbfe4eab84d467703eff1c04d
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-08-29 02:05:16 +0000

    fix: correcting routes of check-ins

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -10,8 +10,8 @@ import { metrics } from './metrics'
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
 
-  app.post('check-ins/:gymId', create)
-  app.patch('check-ins/history', history)
-  app.patch('check-ins/metrics', metrics)
-  app.patch('check-ins/:checkInId/validate', validate)
+  app.post('/check-ins/:gymId', create)
+  app.patch('/check-ins/history', history)
+  app.patch('/check-ins/metrics', metrics)
+  app.patch('/check-ins/:checkInId/validate', validate)
 }
