b507420f6900c2b5af2e70a35c64ad59e027bce3
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-09-03 13:37:41 +0000

    fix: correct SQL query for fetching nearby gyms with latitude/longitude

--- src/repositories/prisma/prisma-gym-repository.ts
+++ src/repositories/prisma/prisma-gym-repository.ts
@@ -36,8 +36,13 @@ class PrismaGymsRepository implements GymsRepository {
 
   async findManyNearby({ latitude, longitude }: FindManyNearbyParams): Promise<GymType[]> {
     const gyms = await prisma.$queryRaw<GymType[]>`
-      SELECT * FROM gyms
-      WHERE ( 6371 * acos( cos( radians( ${latitude} ) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+      SELECT * FROM gyms g
+        WHERE ( 6371 * acos(
+          cos(radians(${latitude}))
+          * cos(radians(g.latitude))
+          * cos(radians(g.longitude) - radians(${longitude}))
+          + sin(radians(${latitude})) * sin(radians(g.latitude))
+        )) <= 10
     `
 
     return gyms
